d95c2d9634b0d90e7cb0f7d93736b0fca725d78a
weishicoding <sanwind0305@gmail.com>
2024-11-20 22:24:48 +0200

    Add about page title and modify color

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import PageHerder from "@/components/PageHerder";
+import React from "react";
 
+const title = "About me";
+const description =
+  "Hi, I'm a full-stack developer who simply loves to code. Whether it's building dynamic websites, solving complex challenges, or just experimenting with new tech, I find joy in every line of code I write. For me, coding isn't just a job—it's a way to bring ideas to life and keep learning something new every day.  I’m always eager to learn, grow, and collaborate with others who share the same passion for tech.";
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <PageHerder
+        title={title}
+        description={description}
+        colorFrom="from-purple-600"
+        colorTo="to-pink-400"
+      />
+    </>
+  );
+};
 
-export default About
+export default About;
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,18 +4,13 @@ const title = "All posts";
 const description =
   "This blog is my space to reflect on the challenges we all face and the lessons we learn along the way. My hope is that by sharing my experiences, I can help others find clarity, inspiration, and support when they need it most.";
 
-type Props = {
-  params: Record<string, never>;
-  searchParams: Record<string, never>;
-};
-
 const Blog = () => {
   return (
     <>
       <PageHerder
         title={title}
         description={description}
-        colorFrom="from-blue-600"
+        colorFrom="from-pink-600"
         colorTo="to-purple-400"
       />
     </>
