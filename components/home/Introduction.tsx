45d7f86456959bceaafb33778aa6e6a2f51bf130
weishicoding <sanwind0305@gmail.com>
2024-12-28 10:55:38 +0200

    change some styles

--- components/home/Introduction.tsx
+++ components/home/Introduction.tsx
@@ -16,8 +16,8 @@ const Introduction = () => {
           <div className="h-60 bg-gradient-to-br from-primary to-purple-400 blur-3xl dark:from-purple-700"></div>
           <div className="h-72 rounded-full bg-gradient-to-r from-cyan-400 to-sky-300 blur-3xl dark:from-transparent dark:to-indigo-600"></div>
         </div>
-        <div className="md:flex justify-between mt-12">
-          <div className="text-center items-center md:text-start">
+        <div className="flex flex-col md:flex-row justify-between mt-12">
+          <div className="text-center items-center md:text-start  order-2 md:order-none">
             {/* Name */}
             <h1 className="bg-gradient-to-b from-black via-black/90 to-black/70 to-90% bg-clip-text text-4xl font-bold text-transparent dark:from-white dark:via-white/80 dark:to-white/60 md:text-7xl">
               Will Shi
@@ -29,19 +29,16 @@ const Introduction = () => {
               </strong>
             </h2>
             <h2 className="text-accent-7 pt-2 text-xl font-semibold leading-8 sm:text-2xl sm:leading-9 md:pr-12">
-              Base on Helsinki, Finland 🇫🇮
-            </h2>
-            <h2 className="text-accent-7 pt-2 text-xl font-semibold leading-8 sm:text-2xl sm:leading-9 md:pr-12">
               I started learning to code {""}
               <strong className="underline decoration-yellow-400 underline-offset-8">
                 in 2016
               </strong>
             </h2>
             <h2 className="text-accent-7 pt-2 text-xl font-semibold leading-8 sm:text-2xl sm:leading-9 md:pr-12">
-              I have passion in JS/TS
+              I've been working in Back-end in Java and Spring for 7 years.
             </h2>
             <h2 className="text-accent-7 pt-2 text-xl font-semibold leading-8 sm:text-2xl sm:leading-9 md:pr-12">
-              I've been working in Back-end in Java and Spring for 7 years.
+              I have passion in JS/TS
             </h2>
             <div className="mt-12 flex justify-center gap-2 md:justify-start">
               <Button variant="primary" className="font-semibold leading-7">
@@ -52,12 +49,12 @@ const Introduction = () => {
               </Button>
             </div>
           </div>
-          <div className="items-center md:item-start aspect-square w-[260px] h-[400px] md:w-[380px] md:h-[560px]">
+          <div className="flex justify-center items-center md:items-start text-center aspect-square w-[260px] h-[400px] md:w-[380px] md:h-[560px] order-1 md:order-none mx-auto">
             <Image
               src={Profile}
               alt=""
               placeholder="blur"
-              className="rotate-3 rounded-lg drop-shadow-2xl transition-all duration-200 hover:rotate-0"
+              className="md:rotate-3 rounded-lg drop-shadow-2xl transition-all duration-200 md:hover:rotate-0"
             />
           </div>
         </div>
